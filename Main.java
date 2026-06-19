d6c297eaf30950ddb7a8087951dc6aa5a7b5e631
sumesh-25 <sumesh4326@gmail.com>
2026-06-19 21:20:03 +0530

    Features Added

--- Main.java
+++ Main.java
@@ -6,12 +6,14 @@ class Book{
     private String title;
     private String author;
     private User Borrower;
+    private Date issueDate;
 
     Book(int book_id,String title, String author){
         this.book_id=book_id;
         this.title=title;
         this.author=author;
         this.Borrower = null;
+        this.issueDate = null;
     }
 
     int getId(){
@@ -26,8 +28,39 @@ class Book{
     User getBorrower(){
         return Borrower;
     }
+    Date getIssueDate(){
+        return issueDate;
+    }
     void setBorrower(User borrower){
         this.Borrower = borrower;
+    }
+    void setIssueDate(Date issueDate){
+        this.issueDate = issueDate;
+    }
+}
+
+class Date{
+    int day;
+    int month;
+    int year;
+    Date(int day, int month, int year){
+        if(day<1 || day>31 || month<1 || month>12 || year<0){
+            System.out.println("Invalid Date");
+        }
+        else{
+            this.day=day;
+            this.month=month;
+            this.year=year;
+        }
+    }
+    int getDay(){
+        return day;
+    }
+    int getMonth(){
+        return month;
+    }
+    int getYear(){
+        return year;
     }
 }
 
@@ -51,7 +84,7 @@ class User{
 }
 
 class Student extends User{
-    String course;
+    private String course;
     Student(int userId, String name, String course){
         super(userId,name);
         this.course=course;
@@ -60,7 +93,7 @@ class Student extends User{
 }
 
 class Teacher extends User{
-    String department;
+    private String department;
     Teacher(int userId, String name, String department){
         super(userId,name);
         this.department=department;
@@ -93,6 +126,14 @@ class Library{
         }
     }
 
+    void searchUsers(String name){
+        name = name.toLowerCase();
+        System.out.println("Searching...");
+        for(User u : users.values()){
+            if(u.getName().toLowerCase().contains(name)) System.out.println("UserId: "+u.getUserId()+"\nName: "+u.getName());
+        }
+    }
+
     void displayUsers(){
         for(User u : users.values()){
             System.out.println("UserId: "+u.getUserId()+"\nName: "+u.getName());
@@ -112,8 +153,13 @@ class Library{
 
     void removeBook(int bookId){
         if(books.containsKey(bookId)){
-            books.remove(bookId);
-            System.out.println("Book Revomed.");
+            if(books.get(bookId).getBorrower()!=null){
+                System.out.println("Book Cannot be removed While Issued.");
+            }
+            else{
+                books.remove(bookId);
+                System.out.println("Book Removed.");
+            }
         }
         else{
             System.out.println("Book Not Found.");
@@ -128,7 +174,7 @@ class Library{
             String borrowerName;
             if(b.getBorrower()!=null) borrowerName = b.getBorrower().getName();
             else borrowerName = "None";
-            if(b.getTitle().toLowerCase().contains(name)) System.out.println("BookId: "+b.getId()+"\nTitle: "+b.getTitle()+"\nAuthor: "+b.getAuthor()+"\nIssued to: "+borrowerName);
+            if(b.getTitle().toLowerCase().contains(name)) System.out.println("BookId: "+b.getId()+"\nTitle: "+b.getTitle()+"\nAuthor: "+b.getAuthor()+"\nIssued to: "+borrowerName );
         }
     }
 
@@ -138,16 +184,33 @@ class Library{
         }
         for(Book b : books.values()){
             String borrowerName; if(b.getBorrower()==null) borrowerName = "none"; else borrowerName = b.getBorrower().getName();
-            System.out.println("ID: "+b.getId()+"\nTitle: "+b.getTitle()+"\nAuthor: "+b.getAuthor()+"\nIssued to: "+borrowerName);
+            System.out.println("Book Id: "+b.getId()+"\nTitle: "+b.getTitle()+"\nAuthor: "+b.getAuthor()+"\nIssued to: "+borrowerName);
         }
         
     }
 
-    void issueBook(int bookId, int userId){
+    void displayIssuedBooks(){
+        for(Book b : books.values()){
+            if(b.getBorrower()!=null)
+                System.out.println("Book Id: "+b.getId()+"\nTitle: "+b.getTitle()+"\nAuthor: "+b.getAuthor()+"\nIssued to: "+b.getBorrower().getName()+"\nIssue Date: "+b.getIssueDate().getDay()+"/"+b.getIssueDate().getMonth()+"/"+b.getIssueDate().getYear());
+        }
+        
+    }
+
+    void displayAvailableBooks(){
+        for(Book b : books.values()){
+            if(b.getBorrower()==null)
+                System.out.println("Book Id: "+b.getId()+"\nTitle: "+b.getTitle()+"\nAuthor: "+b.getAuthor());
+        }
+        
+    }
+
+    void issueBook(int bookId, int userId, Date issueDate){
         if(books.containsKey(bookId)){
             if(books.get(bookId).getBorrower()==null){
                 if(users.containsKey(userId)){
                     books.get(bookId).setBorrower(users.get(userId));
+                    books.get(bookId).setIssueDate(issueDate);
                     System.out.println("Book Issued");
                 }
                 else System.out.println("User Don't Exist");
@@ -161,11 +224,26 @@ class Library{
         if(books.containsKey(bookId)){
             if(books.get(bookId).getBorrower()!=null){
                 books.get(bookId).setBorrower(null);
+                books.get(bookId).setIssueDate(null);
                 System.out.println("Book Returned");
             }
             else System.out.println("Book Already Returned");
         }
         else System.out.println("Book Not Found");
+    }
+
+    void UserBorrowedBooks(int userId){
+        if(users.containsKey(userId)){
+            for(Book b : books.values()){
+                if(b.getBorrower()!=null && b.getBorrower().getUserId()==userId){
+                    System.out.println("Book Id: "+b.getId()+"\nTitle: "+b.getTitle()+"\nAuthor: "+b.getAuthor()+"\nIssue Date: "+b.getIssueDate().getDay()+"/"+b.getIssueDate().getMonth()+"/"+b.getIssueDate().getYear());
+                }
+            }
+        }
+        else{
+            System.out.println("User Not Found");
+            return;
+        }
     }
 
 
@@ -178,7 +256,7 @@ public class Main {
         while (true) {
             int userId;
             int bookId;
-            System.out.print("1.Add Student\n2.Add Teacher\n3.Display Users\n4.Add Book\n5.Remove Book\n6.Search Book\n7.Display Books\n8.Issue Book\n9.Return Book\n10.Exit\n Choice: ");
+            System.out.print("1.Add Student\n2.Add Teacher\n3.Search User \n4.Display Users\n5.Add Book\n6.Remove Book\n7.Search Book\n8.Display Books\n9.Issue Book\n10.Return Book\n11.Display Issued Books\n12.Display Available Books\n13.Books Issued to User\n14.Exit\n Choice: ");
             int choice = sc.nextInt();
             switch (choice){
                 
@@ -203,10 +281,16 @@ public class Main {
                     l.addTeacher(userId, name, department);
                     break;
                 case 3:
+                    System.out.print("Enter User Name: ");
+                    sc.nextLine();
+                    String uname = sc.nextLine();
+                    l.searchUsers(uname);
+                    break;
+                case 4:
                     System.out.print("Users Using the library: \n");
                     l.displayUsers();
                     break;
-                case 4:
+                case 5:
                     System.out.print("Enter Book Id: ");
                     bookId = sc.nextInt();
                     sc.nextLine();
@@ -219,38 +303,60 @@ public class Main {
                     l.addBook(bookId,title,author);
                     break;
                 
-                case 5:
+                case 6:
                     System.out.print("Enter Book Id: ");
                     bookId = sc.nextInt();
                     l.removeBook(bookId);
                     break;
                     
-                case 6:
+                case 7:
                     System.out.print("Enter Book name: ");
-                    String bname = sc.next();
+                    sc.nextLine();
+                    String bname = sc.nextLine();
                     l.searchBooks(bname); 
                     break;
                     
-                case 7:
+                case 8:
                     System.out.println("Books Present in the Library.");
                     l.displayBooks();
-                    break;
-
-                case 8:
-                    System.out.print("Enter Book Id: ");
-                    bookId=sc.nextInt();
-                    System.out.print("Enter User Id: ");
-                    userId = sc.nextInt();
-                    l.issueBook(bookId,userId);
                     break;
 
                 case 9:
                     System.out.print("Enter Book Id: ");
                     bookId=sc.nextInt();
-                    l.returnBook(bookId);
+                    System.out.print("Enter User Id: ");
+                    userId = sc.nextInt();
+                    System.out.print("Enter Issue date (DD MM YYYY): ");
+                    int day = sc.nextInt();
+                    int month = sc.nextInt();
+                    int year = sc.nextInt();
+                    Date issueDate = new Date(day, month, year);
+                    l.issueBook(bookId,userId,issueDate);
                     break;
 
                 case 10:
+                    System.out.print("Enter Book Id: ");
+                    bookId=sc.nextInt();
+                    l.returnBook(bookId);
+                    break;
+
+                case 11:
+                    System.out.println("Books Issued:");
+                    l.displayIssuedBooks();
+                    break;
+
+                case 12:
+                    System.out.println("Books Available:");
+                    l.displayAvailableBooks();
+                    break;
+
+                case 13:
+                    System.out.println("Enter User Id: ");
+                    userId = sc.nextInt();
+                    System.out.println("Books Borrowed by User Id: "+userId);
+                    l.UserBorrowedBooks(userId);
+                    break;
+                case 14:
                     System.out.println("Exiting...");
                     return;
                 default:
